23583aa62dbdbe27953f75001b4ae885a15f3d8e
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2024-04-01 19:49:34 +1000

    Simple vue

--- projects/simple-vue/src/App.vue
+++ projects/simple-vue/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vusdfsse" />
+  <HelloWorld msg="Vite + Svel nkskskkova te sse" />
 </template>
 
 <style scoped>
