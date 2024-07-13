c1c14c83591f4c9787e95974157d7da998fa83c0
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2024-07-13 10:31:21 +1000

    Pattern issue

--- playground/html/address.js
+++ playground/html/address.js
@@ -1,26 +1,43 @@
 function adjustErrorMessageForValidation(event) {
-  const $el = $(event.target);
-  const value = $el.val().trim();
-  if ($el.prop("type") === "text" && value) {
-    const msg = makeMessageForRegexField($el);
-    if (msg) {
-      const fieldName = $el.attr("name");
-      $el.closest("form").validate().settings.messages[fieldName]["regex"] =
-        msg;
+    const $el = $(event.target)
+    const value = $el.val().trim()
+    if ($el.prop('type') === 'text' && value) {
+        const msg = makeMessageForRegexField($el)
+        if (msg) {
+            const fieldName = $el.attr('name')
+            $el.closest('form').validate().settings.messages[fieldName]['regex'] = msg
+        }
     }
-  }
 }
 
 function makeMessageForRegexField($element) {
-  const errorMessageTemplate = $element.data("val-regex");
-  if (!errorMessageTemplate) return null;
+    const errorMessageTemplate = $element.data('val-regex')
+    if (!errorMessageTemplate) return null
 
-  const value = $element.val().trim();
-  const { regex: pattern } = $element.rules();
-  const regex = new RegExp(pattern.replace(/['"]/g, "/"));
+    const { regex: pattern } = $element.rules()
+    if (!pattern) return null
+    const value = $element.val().trim()
 
-  if (regex.test(value)) return null;
+    const regex = new RegExp(pattern.replace(/['"]/g, '/'))
 
-  const problematicChar = value.slice(-1);
-  return errorMessageTemplate.replace("{}", `"${problematicChar}"`);
+    if (regex.test(value)) return null
+
+    const problematicChar = value.slice(-1)
+    return errorMessageTemplate.replace('{}', `"${problematicChar}"`)
+}
+
+// refactored please review
+
+function makeMessageForRegexField($element) {
+    const errorMessageTemplate = $element.data('val-regex')
+    const { regex: pattern } = $element.rules()
+    const value = $element.val().trim()
+
+    if (!errorMessageTemplate || !pattern || !value) return null
+
+    const regex = new RegExp(pattern.replace(/['"]/g, '/'))
+    if (regex.test(value)) return null
+
+    const problematicChar = value.slice(-1)
+    return errorMessageTemplate.replace('{}', `"${problematicChar}"`)
 }
