505f51846644f6624094ca35352e034e9f07747d
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2024-11-08 13:48:56 +1000

    Add login

--- projects/react/src/App.tsx
+++ projects/react/src/App.tsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 import Home from './Pages/Home'
+import Login from './Pages/Login'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [user, setUser] = useState(null)
 
-    return (
-        <>
-            <Home />
-        </>
-    )
+    return <>{user === null ? <Login /> : <Home />}</>
 }
 
 export default App
---
+++ projects/react/src/Pages/Login/index.tsx
@@ -0,0 +1,14 @@
+const index = () => {
+    return (
+        <div className='login'>
+            <h2>Login page</h2>
+            <form>
+                <input type='text' placeholder='Username' />
+                <input type='password' placeholder='Password' />
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default index
