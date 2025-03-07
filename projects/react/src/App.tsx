721d01b50ff428a3977c671709745e53a5dfe535
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2025-03-07 21:01:03 +1000

    Pass login success

--- projects/react/src/App.tsx
+++ projects/react/src/App.tsx
@@ -7,10 +7,14 @@ import './App.css'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 
+const handleOnLoginSuccess = (user) => {
+    console.log("Logged in successfully")
+}
+
 function App() {
     const [user, setUser] = useState(null)
 
-    return <div className='md:container md:mx-auto'>{user === null ? <Login /> : <Home />}</div>
+    return <div className='md:container md:mx-auto'>{user === null ? <Login onSuccess={handleOnLoginSuccess}/> : <Home />}</div>
 }
 
 export default App
--- projects/react/src/Pages/Login/index.tsx
+++ projects/react/src/Pages/Login/index.tsx
@@ -1,19 +1,36 @@
 import TptInput from '../../lib-components/form/Input'
 import React, { useState } from 'react'
 
-const index = () => {
+const index = ({onSuccess}) => {
     const [username, setUsername] = useState('melvin')
+    const [password, setPassword] = useState('1234')
 
     const updateUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUsername(e.target.value)
     }
+    const updatePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(username)
+        console.log(password)
+        if(true) {
+            onSuccess('user')
+        }
+    }
+
     return (
         <div className='login w-80 bg-purple-100  rounded-2xl p-4'>
             <h2 className='text-xl font-semi-bold mb-2'>Login page</h2>
             <form className='py-4 px-4'>
                 <TptInput onChangeHandler={updateUsername} type='text' placeholder='Username' />
-                <TptInput onChangeHandler={updateUsername} type='password' placeholder='Password' />
-                <button className='border-1 px-4 py-2 text-white bg-indigo-400 border-indigo-300 rounded-sm'>
+                <TptInput onChangeHandler={updatePassword} type='password' placeholder='Password' />
+                <button
+                    className='border-1 px-4 py-2 text-white bg-indigo-400 border-indigo-300 rounded-sm'
+                    onClick={handleSubmit}
+                >
                     Login
                 </button>
             </form>
